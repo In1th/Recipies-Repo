7a510d31f894f02221c558bf0a87f5dfd7607d2f
Mateusz Kruk <krukm634@gmail.com>
2024-04-14 23:26:40 +0200

    ci: hmr on frontend through docker

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,6 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		port: 3000,
+		host: true,
+		strictPort: true,
+		hmr: {
+			port: 3010
+		},
+		watch: {
+			usePolling: true
+		}
+	},
 	plugins: [sveltekit()],
 });
 

7bd6db5d9ee09f222c6fc4d40a1cd039b6a15804
teatro <teatrocamillo@gmail.com>
2024-06-25 18:10:35 +0200

    search basic

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,27 +1,15 @@
 <script>
     import RecipeGrid  from "$lib/components/recipe/RecipeGrid.svelte";
-    import RecipeHighlight  from "$lib/components/recipe/RecipeHighlight.svelte";
-
-
     import { searchHandler, searchTermsRecipe, searchStore } from '$lib/components/searchbar/search';
     import { onDestroy } from 'svelte';
 
     export let data;
-    console.log("data");
-    console.log(data.recipes);
-
     const searchRecipes = searchTermsRecipe(data.recipes);
-    console.log("searchRecipes");
-    console.log(searchRecipes);
-
     searchStore.set({
         data: searchRecipes,
         filtered: searchRecipes,
         search: ""
     });
-
-    console.log("searchStore filtered");
-    console.log($searchStore.filtered);
 
     const unsubscribe = searchStore.subscribe((model) => searchHandler(model, model.search));
 
--- front/src/routes/recipes/+page.server.ts
+++ front/src/routes/recipes/+page.server.ts
@@ -2,8 +2,6 @@ import type { PageServerLoad } from "./$types";
 import { fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async () => {
-
-    console.log("getting data page !");
     const res = await fetch("http://backend:8080/api/v0/recipes");
 
     if (!res.ok) {
@@ -14,9 +12,6 @@ export const load: PageServerLoad = async () => {
     }
 
     const data = await res.json();
-
-    console.log("data json:");
-    console.log(data);
 
     return {
         data

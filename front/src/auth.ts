dd7eb1e8b75842c6ec27f056792103e957ceca17
Mateusz Kruk <krukm634@gmail.com>
2024-07-22 00:05:46 +0200

    Update auth.ts

--- front/src/auth.ts
+++ front/src/auth.ts
@@ -4,6 +4,7 @@ import  { SvelteKitAuth } from "@auth/sveltekit"
 export const { signIn, signOut, handle } = SvelteKitAuth({
   providers: [Auth0({
     issuer: "https://dev-ceqe1dwsh47i6qg4.us.auth0.com",
+    
     profile(profile) {
       return {
         id: profile.user_id,

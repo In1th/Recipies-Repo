89b720fbc22d24c24f0dda7f445dcb4bd2c854d3
teatro <teatrocamillo@gmail.com>
2024-09-09 18:26:42 +0200

    smooth page

--- front/src/routes/recipes/+page.svelte
+++ front/src/routes/recipes/+page.svelte
@@ -1,7 +1,21 @@
 <script>
-    import RecipeGrid from "$lib/components/recipe/RecipeGrid.svelte";
-    import { searchStore } from "$lib/components/searchbar/search";
+    import RecipeGrid  from "$lib/components/recipe/RecipeGrid.svelte";
+    import { searchHandler, searchTermsRecipe, searchStore } from '$lib/components/searchbar/search';
+    import { onDestroy } from 'svelte';
 
+    // export let data;
+    // const searchRecipes = searchTermsRecipe(data.recipes);
+    // searchStore.set({
+    //     data: searchRecipes,
+    //     filtered: searchRecipes,
+    //     search: ""
+    // });
+
+    // const unsubscribe = searchStore.subscribe((model) => searchHandler(model, model.search));
+
+    // onDestroy(() => {
+    //     unsubscribe();
+    // });
 </script>
 
 <section class="flex flex-col justify-center items-center">
